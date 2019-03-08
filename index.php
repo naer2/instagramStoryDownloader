ab2cbf4d686f6c13591e8ba9b52abb5deaeca3d5
naer2 <f@fusc.ws>
2019-03-08 13:57:27 +0300

    webUri added

--- index.php
+++ index.php
@@ -12,15 +12,29 @@ try {
     $storyFeed = $ig->story->getUserStoryFeed($userID);
     $storyCount= count($storyFeed->getReel()->getItems());
 
+
+
     for ($i=0; $i < $storyCount; $i++) {
       if ($storyFeed->getReel()->getItems()[$i]->getMediaType()==1) {
     echo "</br><img src='".$storyFeed->getReel()->getItems()[$i]->getImageVersions2()->getCandidates()[0]->getUrl()."' width='480' height='850'></br>";
+    if ($storyFeed->getReel()->getItems()[$i]->getStoryCta()==null) {
+       echo "noWebUri";
+    }
+    else {
+        echo "webUri:".$storyFeed->getReel()->getItems()[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri();
+    }
       }
       else {
         echo "</br><video width='480' height='850' controls>
           <source src='".$storyFeed->getReel()->getItems()[$i]->getVideoVersions()[0]->getUrl()."' type='video/mp4'>
         Your browser does not support the video tag.
         </video></br>";
+        if ($storyFeed->getReel()->getItems()[$i]->getStoryCta()==null) {
+           echo "noWebUri";
+        }
+        else {
+            echo "webUri:".$storyFeed->getReel()->getItems()[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri();
+        }
       }
     }
 
