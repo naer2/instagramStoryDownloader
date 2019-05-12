9ca7aff6943cd465b44224cb393177ed42e31f02
naer2 <f@fusc.ws>
2019-05-12 15:44:13 +0300

    Update 1.2
    code simplify & functions added & story time added

--- index.php
+++ index.php
@@ -1,46 +1,40 @@
 <?php
-
 // **author**
 // naer (http://github.com/naer2/)
-
 require("config.php");
-$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
+require("functions.php");
 
+$ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
 try {
     $ig->login($username, $password);
     $userID = $ig->people->getUserIdForName($showStoryUsername);
     $storyFeed = $ig->story->getUserStoryFeed($userID);
-    $storyCount= count($storyFeed->getReel()->getItems());
-
-
-
+    $storyItems = $storyFeed->getReel()->getItems();
+    $storyCount= count($storyItems);
     for ($i=0; $i < $storyCount; $i++) {
-      if ($storyFeed->getReel()->getItems()[$i]->getMediaType()==1) {
-    echo "</br><img src='".$storyFeed->getReel()->getItems()[$i]->getImageVersions2()->getCandidates()[0]->getUrl()."' width='480' height='850'></br>";
-    if ($storyFeed->getReel()->getItems()[$i]->getStoryCta()==null) {
-       echo "noWebUri";
-    }
-    else {
-        echo "webUri:".$storyFeed->getReel()->getItems()[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri();
-    }
+      //img
+      echo time_elapsed_string("@".$storyItems[$i]->getTakenAt());
+      if ($storyItems[$i]->getStoryCta()==null) {
+         echo " ";
       }
       else {
-        echo "</br><video width='480' height='850' controls>
-          <source src='".$storyFeed->getReel()->getItems()[$i]->getVideoVersions()[0]->getUrl()."' type='video/mp4'>
-        Your browser does not support the video tag.
-        </video></br>";
-        if ($storyFeed->getReel()->getItems()[$i]->getStoryCta()==null) {
-           echo "noWebUri";
+          echo " & <a href='".$storyItems[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri()."'>webUri</a>";
+      }
+      if ($storyItems[$i]->getMediaType()==1) {
+        getImgHTML($storyItems[$i]->getImageVersions2()->getCandidates()[0]->getUrl());
+      }
+      else {
+        #vids
+        if ($storyItems[$i]->getStoryCta()==null) {
+           echo " ";
         }
         else {
-            echo "webUri:".$storyFeed->getReel()->getItems()[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri();
+            echo " & <a href='".$storyItems[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri()."'>webUri</a>";
         }
+      getVideoHTML($storyItems[$i]->getVideoVersions()[0]->getUrl());
       }
     }
-
 } catch (\Exception $e) {
     echo 'Something went wrong: '.$e->getMessage()."\n";
     exit(0);
 }
-
-?>
