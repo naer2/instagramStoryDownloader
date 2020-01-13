62da79748d5d1066a1e85051c2e3ee527f617ae6
shel <shelc@alumni.nottingham.ac.uk>
2020-01-13 21:46:39 +0400

    Update index.php

--- index.php
+++ index.php
@@ -12,26 +12,15 @@ try {
     $storyItems = $storyFeed->getReel()->getItems();
     $storyCount= count($storyItems);
     for ($i=0; $i < $storyCount; $i++) {
-      //img
       echo time_elapsed_string("@".$storyItems[$i]->getTakenAt());
-      if ($storyItems[$i]->getStoryCta()==null) {
-         echo " ";
-      }
-      else {
-          echo " & <a href='".$storyItems[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri()."'>webUri</a>";
+      if ($storyItems[$i]->getStoryCta()!=null) {
+          echo " & <a href='".$storyItems[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri()."'>webUri</a>".PHP_EOL;
       }
       if ($storyItems[$i]->getMediaType()==1) {
         getImgHTML($storyItems[$i]->getImageVersions2()->getCandidates()[0]->getUrl());
       }
       else {
-        #vids
-        if ($storyItems[$i]->getStoryCta()==null) {
-           echo " ";
-        }
-        else {
-            echo " & <a href='".$storyItems[$i]->getStoryCta()[0]->getLinks()[0]->getWebUri()."'>webUri</a>";
-        }
-      getVideoHTML($storyItems[$i]->getVideoVersions()[0]->getUrl());
+        getVideoHTML($storyItems[$i]->getVideoVersions()[0]->getUrl());
       }
     }
 } catch (\Exception $e) {
